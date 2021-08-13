d171b2bd62cca20da3990feb9b3d4174e122f0a5
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-08-13 08:03:25 +0530

    Added: Program to Indian State Census

---
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -0,0 +1,21 @@
+package censusanalyser;
+
+public class CensusAnalyserException extends Exception {
+
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM,
+        INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
